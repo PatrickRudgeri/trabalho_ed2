d26efb362de176e1c2eb2980b56d4090aa92dbc4
Patrick Rudgeri <patrick.rudgeri@ice.ufjf.br>
2020-10-08 16:28:56 -0300

    Adicionando dataset reduzido e padronizado para testes

--- src/Csv.cpp
+++ src/Csv.cpp
@@ -14,10 +14,13 @@ void Csv::lerCsv(string nomeArquivo, int numLinhas, bool aleatorio) {
 
 }
 
+//TODO, implementar essa função para "padronizar" o DF
 string Csv::padronizarCsv(string nomeArquivo) {
-    /*
-     * Utilizar o código em helpers para processar o arquivo inteiro e salvar em outro csv
-     * */
-    return std::string();
+
+    // Utilizar o código em helpers para processar o arquivo inteiro e salvar em outro csv
+
+    // Para continuar o desenvolvimento, criei um dataset manualmente padronizado.
+    // Assim que tudo estiver funcionando volto para implementar essa função
+    return "std_" + nomeArquivo;
 }
 
